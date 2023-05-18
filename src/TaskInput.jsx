b7c0e871d25a645c007d3363a934fef64e5d42fa
michealcoleyoung <coleyoungmusic@gmail.com>
2023-05-18 14:03:40 -0500

    Updated TaskInput component

--- src/TaskInput.jsx
+++ src/TaskInput.jsx
@@ -1,30 +1,36 @@
 import { useState, useEffect } from 'react';
 
 export function TaskInput(props) {
-    const [task, setTask] = useState('');
-    const [tasks, setTasks] = useState([]);
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-    useEffect(() => {
-        console.log(tasks);
-      }, [tasks]);
-  
-    function handleChange(event) {
-      setTask(event.target.value);
-    }
-  
-    function handleSubmit(event) {
-      event.preventDefault();
-      if(task.trim() !== '')
+  useEffect(() => {
+    console.log(tasks);
+  }, [tasks]);
+
+  function handleChange(event) {
+    setTask(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (task.trim() !== '') {
       // Add the task to the tasks array
       setTasks([...tasks, task]);
       // Clears the task input field
       setTask('');
     }
-  
-    return (
+  }
+
+  return (
+    <div>
       <form onSubmit={handleSubmit}>
         <input type="text" value={task} onChange={handleChange} />
         <button type="submit">Add Task</button>
       </form>
-    );
-  }
+      {tasks.map((task, index) => (
+        <div key={index}>{task}</div>
+      ))}
+    </div>
+  );
+}
