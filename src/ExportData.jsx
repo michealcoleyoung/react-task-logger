e3fb58f702ecd2af91a9df9488556c349f615832
michealcoleyoung <coleyoungmusic@gmail.com>
2023-05-27 14:02:15 -0500

    Added placeholders for ExportData SOS

--- src/ExportData.jsx
+++ src/ExportData.jsx
@@ -5,12 +5,31 @@ import { TaskInput } from './TaskInput';
 // Functionality will be included in the main App
 
 export function ExportData() {
+  const [exportedData, setExportedData] = useState('');
+
+  function handleExportClick() {
+
+      // Get the task information from the TaskInput component or any other source
+      const taskName = 'Task 1';
+      const taskStart = '04:06 AM';
+      const timeWorked = '20:53';
+      const taskDate = '05/20/2023';
+      const taskEnd = '04:10 AM';
+  
+      const exportDataString = `Task Name: ${taskName}\nTask Start: ${taskStart}\nTime Worked: ${timeWorked}\nTask Date: ${taskDate}\nTask End: ${taskEnd}`;
+  
+      setExportedData(exportDataString);
+      console.log('Test message')
+
+  }
+
+ 
 
   return (
     <div>
-      <button >Export Data</button><br></br><br></br>
+      <button onClick={handleExportClick}>Export Data</button><br></br><br></br>
       <textarea rows={20} cols={50}>
-
+        {exportedData}
       </textarea>
     </div>
   );
